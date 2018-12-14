6846b9c480df5e0f954aa39319bce992d62181c6
jarikomppa <jari.komppa@gmail.com>
2018-12-14 18:55:00 +0200

    Render buffer (render to texture)

---
+++ demos/renderbuf.cpp
@@ -0,0 +1,262 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <SDL.h>
+#include "glad/include/glad/glad.h"
+#define GLM_ENABLE_EXPERIMENTAL
+#include "/libraries/glm/glm/glm.hpp"
+#include "/libraries/glm/glm/gtx/transform.hpp"
+#include "/libraries/glm/glm/gtx/rotate_vector.hpp"
+#include "/libraries/glm/glm/gtc/type_ptr.hpp"
+#define STB_IMAGE_IMPLEMENTATION
+#include "../stb/stb_image.h"
+
+#define ST_H_IMPLEMENTATION
+#include "../st_shader.h"
+#include "../st_vertexbuffer.h"
+#include "../st_generate_cube.h"
+#include "../st_renderbuf.h"
+#include "../st_texture.h"
+
+using namespace st;
+
+#define TITLE "ShaderToolkit Demo"
+// For windowed mode
+#define DESIRED_WINDOW_WIDTH (1920/2)
+// For windowed mode
+#define DESIRED_WINDOW_HEIGHT (1080/2)
+// Desired aspect ratio. 640x480 = 4/3. Comment out to use whatever the desktop is set.
+#define DESIRED_ASPECT ((float)(DESIRED_WINDOW_WIDTH)/(float)(DESIRED_WINDOW_HEIGHT))
+
+//#define FULLSCREEN_BY_DEFAULT
+
+int gScreenWidth = 0;
+int gScreenHeight = 0;
+SDL_Window *gSDLWindow;
+
+void initvideo(int argc)
+{
+	int bpp = 0;
+	int flags = 0;
+	SDL_Rect info;
+	SDL_GetDisplayBounds(0, &info);
+
+#ifdef _DEBUG
+	int fsflag = 0;
+#else
+#ifdef FULLSCREEN_BY_DEFAULT
+	int fsflag = 1;
+#else
+	int fsflag = 0;
+#endif
+#endif
+
+	if (argc > 1) fsflag = !fsflag;
+
+	if (fsflag)
+	{
+		SDL_ShowCursor(0); // hide cursor in full screen
+		gScreenWidth = info.w;
+		gScreenHeight = info.h;
+		flags = SDL_WINDOW_OPENGL | SDL_WINDOW_SHOWN | SDL_WINDOW_FULLSCREEN;
+	}
+	else
+	{
+		gScreenWidth = DESIRED_WINDOW_WIDTH;
+		gScreenHeight = DESIRED_WINDOW_HEIGHT;
+		flags = SDL_WINDOW_OPENGL | SDL_WINDOW_SHOWN;
+	}
+
+	SDL_GL_SetAttribute(SDL_GL_RED_SIZE, 8);
+	SDL_GL_SetAttribute(SDL_GL_GREEN_SIZE, 8);
+	SDL_GL_SetAttribute(SDL_GL_BLUE_SIZE, 8);
+	SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 16);
+	SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+
+	gSDLWindow = SDL_CreateWindow(
+		"",
+		SDL_WINDOWPOS_CENTERED,
+		SDL_WINDOWPOS_CENTERED,
+		gScreenWidth,
+		gScreenHeight,
+		flags);
+
+	if (gSDLWindow == 0)
+	{
+		fprintf(stderr, "Video mode set failed: %s\n", SDL_GetError());
+		SDL_Quit();
+		exit(0);
+	}
+
+	SDL_GLContext glcontext = SDL_GL_CreateContext(gSDLWindow);
+
+	SDL_GL_SetSwapInterval(1);
+
+	if (!gladLoadGL()) {
+		printf("Something went wrong!\n");
+		exit(-1);
+	}
+
+#ifdef DESIRED_ASPECT
+	float aspect = DESIRED_ASPECT;
+	if (((float)gScreenWidth / gScreenHeight) > aspect)
+	{
+		float realx = gScreenHeight * aspect;
+		float extrax = gScreenWidth - realx;
+
+		glViewport((GLsizei)(extrax / 2), 0, (GLsizei)realx, (GLsizei)gScreenHeight);
+	}
+	else
+	{
+		float realy = gScreenWidth / aspect;
+		float extray = gScreenHeight - realy;
+
+		glViewport(0, (GLsizei)(extray / 2), (GLsizei)gScreenWidth, (GLsizei)realy);
+	}
+#else
+	glViewport(0, 0, (GLsizei)gScreenWidth, (GLsizei)gScreenHeight);
+#endif
+}
+
+void process_events()
+{
+	SDL_Event event;
+
+	while (SDL_PollEvent(&event))
+	{
+		switch (event.type)
+		{
+		case SDL_KEYDOWN:
+//			handle_key(event.key.keysym.sym, 1);
+			break;
+		case SDL_KEYUP:
+			exit(0);
+//			handle_key(event.key.keysym.sym, 0);
+			break;
+		case SDL_QUIT:
+			SDL_Quit();
+			exit(0);
+			break;
+		case SDL_WINDOWEVENT_RESIZED:
+			gScreenWidth = event.window.data1;
+			gScreenHeight = event.window.data2;
+			initvideo(0);
+			break;
+		}
+	}
+}
+
+//////////////////////////////////////////////////////////////////////////
+
+Vertexbuffer *cube;
+Shader flat;
+Renderbuf renderbuf;
+GLuint tex;
+
+void init()
+{
+	tex = loadtexture("texture.png");
+	renderbuf.init();
+	cube = generate_cube(50, 20, 30);
+	flat.loadmem((char*)
+		"#version 330\n"
+		"#extension GL_ARB_explicit_uniform_location : enable\n"
+		"\n"
+		"layout(location = 1) in vec4 vertexposition;\n"
+		"layout(location = 3) in vec2 texcoord;\n"
+		"\n"
+		"uniform mat4 mvp;\n"
+		"out vec2 uv;\n"
+		"\n"
+		"void main()\n"
+		"{\n"
+		"	gl_Position = mvp * vertexposition;\n"
+		"	uv = texcoord;\n"
+		"}\n",
+		(char*)
+		"#version 330\n"
+		"#extension GL_ARB_explicit_uniform_location : enable\n"
+		"\n"
+		"uniform sampler2D tex;"
+		"in vec2 uv;\n"
+		"out vec4 fragcolor;\n"
+		"\n"
+		"void main()\n"
+		"{\n"
+		"	fragcolor = texture(tex, uv.xy);\n"
+		"}\n");
+}
+
+
+void draw_screen()
+{
+	glClearColor(0, 0, 1, 1);
+	glClear(GL_COLOR_BUFFER_BIT);
+	glEnable(GL_CULL_FACE);
+	int tick = SDL_GetTicks();
+
+	renderbuf.enable();
+	{
+		glClearColor(1, 0, 0, 1);
+		glClear(GL_COLOR_BUFFER_BIT);
+		glm::mat4 lookat = glm::lookAt(glm::vec3(sin(tick * 0.000345)*100.0f, 30, cos(tick * 0.000345)*100.0f), glm::vec3(0, 0, 0), glm::vec3(0, 1, 0));
+		glm::mat4 proj = glm::perspective(45.0f, gScreenWidth / (float)gScreenHeight, 1.0f, 1000.0f);
+		glm::mat4 mvp = proj * lookat;
+
+		flat.enable();
+		glUniformMatrix4fv(flat.getUniformLocation("mvp"), 1, 0, glm::value_ptr(mvp));
+		glUniform1i(flat.getUniformLocation("tex"), 0);
+		glActiveTexture(GL_TEXTURE0);
+		glBindTexture(GL_TEXTURE_2D, tex);
+
+		cube->enable();
+		cube->render();
+		cube->disable();
+		flat.disable();
+	}
+	renderbuf.disable();
+
+	glm::mat4 lookat = glm::lookAt(glm::vec3(sin(tick * 0.000345)*100.0f, 30, cos(tick * 0.000345)*100.0f), glm::vec3(0, 0, 0), glm::vec3(0, 1, 0));
+	glm::mat4 proj = glm::perspective(45.0f, gScreenWidth / (float)gScreenHeight, 1.0f, 1000.0f);
+	glm::mat4 mvp = proj * lookat;
+
+	flat.enable();
+	glUniformMatrix4fv(flat.getUniformLocation("mvp"), 1, 0, glm::value_ptr(mvp));
+	glUniform1i(flat.getUniformLocation("tex"), 0);
+	glActiveTexture(GL_TEXTURE0);
+	glBindTexture(GL_TEXTURE_2D, renderbuf.mRenderTex);
+
+	cube->enable();
+	cube->render();
+	cube->disable();
+	flat.disable();
+	SDL_Delay(1);
+	SDL_GL_SwapWindow(gSDLWindow);
+}
+
+//////////////////////////////////////////////////////////////////////////
+
+int main(int argc, char** args)
+{
+
+	if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_NOPARACHUTE) < 0)
+	{
+		fprintf(stderr, "Video initialization failed: %s\n", SDL_GetError());
+		SDL_Quit();
+		exit(0);
+	}
+
+	initvideo(argc);
+
+	// set window title
+	SDL_SetWindowTitle(gSDLWindow, TITLE " - http://iki.fi/sol/");
+
+	init();
+
+	while (1)
+	{
+		process_events();
+		draw_screen();
+	}
+
+	return 0;
+}
---
+++ st_renderbuf.h
@@ -0,0 +1,169 @@
+// by Jari Komppa 2018
+// Under unlicense. Google it.
+// Use ST_RENDERBUF_H_IMPLEMENTATION in one source file before including this.
+// (Or if you're using a bunch of ST_ headers and want to implement all, just use ST_H_IMPLEMENTATION)
+// Needs opengl headers before it, naturally.
+
+#ifndef ST_RENDERBUF_H
+#define ST_RENDERBUF_H
+
+namespace st
+{
+
+	class Renderbuf
+	{
+	public:
+		GLuint mRenderFBO, mRenderRB, mRenderTex;
+		GLint mPrevFB;
+		GLint mPrevViewport[4];
+		int mRenderTextureDimension;
+
+		void init(int aRenderTextureDimension = 1024);
+		void enable();
+		void disable();
+		virtual ~Renderbuf();
+		void check_gl_error(const char * fn, int l);
+		Renderbuf();
+	};
+}
+
+#if defined(ST_RENDERBUF_H_IMPLEMENTATION) || defined(ST_H_IMPLEMENTATION)
+
+#define RENDERBUF_H_CHECK_GL_ERROR check_gl_error(__FILE__,__LINE__);
+
+namespace st
+{
+	void Renderbuf::check_gl_error(const char * fn, int l)
+	{
+		int err = glGetError();
+		if (err != GL_NO_ERROR)
+		{
+			err = err;
+			char * errst = (char*)"";
+			char * enumst = (char*)"";
+			switch (err)
+			{
+			case GL_NO_ERROR: enumst = (char*)"GL_NO_ERROR"; errst = (char*)
+				"No error has been recorded. "
+				"The value of this symbolic constant is guaranteed to be 0.";
+				break;
+
+			case GL_INVALID_ENUM: enumst = (char*)"GL_INVALID_ENUM"; errst = (char*)
+				"An unacceptable value is specified for an enumerated argument. "
+				"The offending command is ignored "
+				"and has no other side effect than to set the error flag.";
+				break;
+
+			case GL_INVALID_VALUE: enumst = (char*) "GL_INVALID_VALUE"; errst = (char*)
+				"A numeric argument is out of range. "
+				"The offending command is ignored "
+				"and has no other side effect than to set the error flag.";
+				break;
+
+			case GL_INVALID_OPERATION: enumst = (char*)"GL_INVALID_OPERATION"; errst = (char*)
+				"The specified operation is not allowed in the current state. "
+				"The offending command is ignored "
+				"and has no other side effect than to set the error flag.";
+				break;
+
+			case GL_STACK_OVERFLOW: enumst = (char*)"GL_STACK_OVERFLOW"; errst = (char*)
+				"This command would cause a stack overflow. "
+				"The offending command is ignored "
+				"and has no other side effect than to set the error flag.";
+				break;
+
+			case GL_STACK_UNDERFLOW: enumst = (char*)"GL_STACK_UNDERFLOW"; errst = (char*)
+				"This command would cause a stack underflow. "
+				"The offending command is ignored "
+				"and has no other side effect than to set the error flag.";
+				break;
+
+			case GL_OUT_OF_MEMORY: enumst = (char*)"GL_OUT_OF_MEMORY"; errst = (char*)
+				"There is not enough memory left to execute the command. "
+				"The state of the GL is undefined, "
+				"except for the state of the error flags, "
+				"after this error is recorded.";
+				break;
+				/*
+			case GL_TABLE_TOO_LARGE: enumst = "GL_TABLE_TOO_LARGE"; errst =
+				"The specified table exceeds the implementation's maximum supported table "
+				"size.  The offending command is ignored and has no other side effect "
+				"than to set the error flag.";
+				break;
+				*/
+			default:
+				enumst = (char*)"Unknown"; errst = (char*)"Unknown gl error";
+				break;
+			}
+			char tmp[256];
+			sprintf_s(tmp, 256, "%s::%d:%s", fn, l, enumst);
+			MessageBoxA(NULL, errst, tmp, MB_OK);
+		}
+	}
+
+	Renderbuf::Renderbuf()
+	{
+		mRenderTex = 0;
+		mRenderFBO = 0;
+		mRenderRB = 0;
+	}
+
+	Renderbuf::~Renderbuf()
+	{
+		if (mRenderTex)
+			glDeleteTextures(1, &mRenderTex);
+		if (mRenderFBO)
+			glDeleteFramebuffers(1, &mRenderFBO);
+		if (mRenderRB)
+			glDeleteRenderbuffers(1, &mRenderRB);
+	}
+
+	void Renderbuf::init(int aRenderTextureDimension)
+	{
+		mRenderTextureDimension = aRenderTextureDimension;
+
+		glActiveTexture(GL_TEXTURE0); RENDERBUF_H_CHECK_GL_ERROR
+		glGenTextures(1, &mRenderTex); RENDERBUF_H_CHECK_GL_ERROR
+		glBindTexture(GL_TEXTURE_2D, mRenderTex); RENDERBUF_H_CHECK_GL_ERROR
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST); RENDERBUF_H_CHECK_GL_ERROR
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST); RENDERBUF_H_CHECK_GL_ERROR
+
+		glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT); RENDERBUF_H_CHECK_GL_ERROR//GL_CLAMP_TO_BORDER);
+		glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT); RENDERBUF_H_CHECK_GL_ERROR// GL_CLAMP_TO_BORDER);
+
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, aRenderTextureDimension, aRenderTextureDimension, 0, GL_RGBA, GL_UNSIGNED_BYTE, 0); RENDERBUF_H_CHECK_GL_ERROR
+
+		glGenFramebuffers(1, &mRenderFBO); RENDERBUF_H_CHECK_GL_ERROR
+		glBindFramebuffer(GL_FRAMEBUFFER, mRenderFBO); RENDERBUF_H_CHECK_GL_ERROR
+
+		glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, mRenderTex, 0); RENDERBUF_H_CHECK_GL_ERROR
+
+		glGenRenderbuffers(1, &mRenderRB);
+		glBindRenderbuffer(GL_RENDERBUFFER, mRenderRB);
+		glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH_COMPONENT24, aRenderTextureDimension, aRenderTextureDimension);
+
+		GLenum status = glCheckFramebufferStatus(GL_FRAMEBUFFER);
+		
+		// .. do something with the status maybe?
+
+		glBindFramebuffer(GL_FRAMEBUFFER, 0); RENDERBUF_H_CHECK_GL_ERROR
+		glActiveTexture(GL_TEXTURE0); RENDERBUF_H_CHECK_GL_ERROR
+	}
+
+	void Renderbuf::enable()
+	{
+		glGetIntegerv(GL_VIEWPORT, &mPrevViewport[0]);
+		glGetIntegerv(GL_DRAW_FRAMEBUFFER_BINDING, &mPrevFB);
+		glBindFramebuffer(GL_FRAMEBUFFER, mRenderFBO); RENDERBUF_H_CHECK_GL_ERROR
+		glViewport(0, 0, mRenderTextureDimension, mRenderTextureDimension); RENDERBUF_H_CHECK_GL_ERROR
+	}
+
+	void Renderbuf::disable()
+	{
+		glViewport(mPrevViewport[0], mPrevViewport[1], mPrevViewport[2], mPrevViewport[3]); RENDERBUF_H_CHECK_GL_ERROR
+		glBindFramebuffer(GL_FRAMEBUFFER, mPrevFB); RENDERBUF_H_CHECK_GL_ERROR
+	}
+}
+
+#endif // implementation
+#endif // include guard
